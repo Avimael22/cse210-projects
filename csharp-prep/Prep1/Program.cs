b0b74811d28abf9b432c67c80ce981cc7768de49
Avimael22 <ghostriley584@gmail.com>
2023-09-14 20:48:52 -0600

    Variables and Input/Output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+         string first_name = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+         string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {first_name}, {first_name} {last_name}.");
     }
 }
