bcc3f012012ade6bc2e1521502382424d0b4a624
Avimael22 <ghostriley584@gmail.com>
2023-09-14 21:42:50 -0600

    Core Challenges completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What was your grade percentage? ");
+        string percentage = Console.ReadLine();
+
+        int grade = int.Parse(percentage);
+        string letter = null;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+           letter = "F";
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations You have passed the course");
+        }
+        else
+        {
+            Console.Write($"Sorry, You have failed the Course.  Dont let this discourage you and prepare even more for the retake");
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+
     }
 }
