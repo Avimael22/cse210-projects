db14c0d0f31a527d0e44e5199e1ec6c38c4accca
Avimael22 <ghostriley584@gmail.com>
2023-09-19 22:41:40 -0600

    completed core requirements and stretchs challenge for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Do you want to play a game? ");
+        string answer = Console.ReadLine();
+        while (answer == "yes"){
+            Random RandomGenerator = new Random();
+            int magicNumber = RandomGenerator.Next(1, 100);
+            int guess;
+            do {
+                Console.WriteLine("What is your guess for the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                if (guess == magicNumber){
+                Console.WriteLine("Congratulations! Your guess was correct!");
+            }
+                else if (guess > magicNumber){
+                    Console.WriteLine("You should guess a little lower!");
+                }
+                else {
+                    Console.WriteLine("You should guess a little higher");
+                }
+                
+            } while(guess != magicNumber);
+             
+            Console.WriteLine("Do you want to continue playing? ");
+            answer = Console.ReadLine();
+
+        
+        
+        }  
     }
 }
