112800458f1d6a34c0807ce2a40ecdb5f5ce4b92
Avimael22 <ghostriley584@gmail.com>
2023-09-23 21:31:00 -0600

    Completed requirements for prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        double square = SquareNumber(number);
+        DisplayResult(name, square);
+
+    }
+    static void DisplayMessage(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName(){
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber(){
+        Console.WriteLine("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static double SquareNumber(int number){
+        double numberSquared = Math.Pow(number, 2);
+        return numberSquared;
+    }
+    static void DisplayResult(string name, double square){
+        Console.WriteLine($"Brother {name}, the square of your number is {square}");
     }
 }
